a29879a1e5dddb082c6d0072bc2a27ff414b14d6
Mikko Poutanen <mikko.poutanen@vincit.fi>
2019-07-17 18:18:21 +0300

    Add Hello World app

---
+++ apps/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Mikko Poutanen on 17/07/2019.
+//  Copyright © 2019 Mikko Poutanen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
