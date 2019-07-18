3ff58cdf9d11fcffec6968e9d996189c40f210cb
Mikko Poutanen <mikko.poutanen@vincit.fi>
2019-07-18 15:07:33 +0300

    Dicee add user interaction

--- apps/Dicee/Dicee/ViewController.swift
+++ apps/Dicee/Dicee/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomDiceIndexLeft = 0;
+    var randomDiceIndexRight = 0;
+    
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDiceImages()
         // Do any additional setup after loading the view.
     }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        randomDiceIndexLeft = Int.random(in: 0...5)
+        randomDiceIndexRight = Int.random(in: 0...5)
+        
+        diceImageViewLeft.image = UIImage(named: "dice\(randomDiceIndexLeft + 1)")
+        diceImageViewRight.image = UIImage(named: "dice\(randomDiceIndexRight + 1)")
+    }
+    
 }
 
