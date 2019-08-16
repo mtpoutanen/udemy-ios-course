ececae0b78f1cae814a299f1682e173d323cb15d
Mikko Poutanen <mikko.poutanen@vincit.fi>
2019-08-16 10:38:29 +0300

    Magic 8 Ball App initial commit with assets

---
+++ apps/Magic 8Ball/Magic 8Ball/Magic 8Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8Ball
+//
+//  Created by Mikko Poutanen on 16/08/2019.
+//  Copyright © 2019 Mikko Poutanen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
