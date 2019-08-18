c4d3e03c093b9d20000b91514bcf5fe0ca466c46
Mikko Poutanen <mikko.poutanen@vincit.fi>
2019-08-18 14:51:02 +0300

    Finish Magic 8 Ball

--- apps/Magic 8Ball/Magic 8Ball/Magic 8Ball/ViewController.swift
+++ apps/Magic 8Ball/Magic 8Ball/Magic 8Ball/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var randomBallNumber = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateImage()
+    }
+    
+    func updateImage() {
+        randomBallNumber = Int.random(in: 0...4)
+        imageView.image = UIImage(named: "ball\(randomBallNumber + 1)")
     }
 
-
+    @IBAction func onAskButtonPress(_ sender: Any) {
+        updateImage()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateImage()
+    }
 }
 
