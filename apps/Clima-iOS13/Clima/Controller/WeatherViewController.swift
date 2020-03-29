011231ac37ed0c7ec1867c6fd6a43aa9edd105cf
Mikko Poutanen <mikko.poutanen@vincit.fi>
2020-03-29 21:34:18 +0300

    Add basic UI elements and UITextField to Clima App

--- apps/Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ apps/Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,44 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
 
-
+    func handleSearch () {
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+    }
+    
+    @IBAction func onSearchPress(_ sender: Any) {
+        handleSearch()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        handleSearch()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            textField.placeholder = "Search"
+            return true
+        } else {
+            textField.placeholder = "Yo, type sumtin'"
+        }
+        return false
+    }
 }
 
